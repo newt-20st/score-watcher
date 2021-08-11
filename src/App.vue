4df6e606947a6fa079de4b96e651f2f1135ee73b
newt <ysti2019.win2020.st@gmail.com>
2021-08-11 22:08:16 +0900

    add: first commit
    Vue3とElectronを用いてメモ帳アプリを作成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -21,6 +20,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height:100%;
+}
+body{
+  height:100vh;
+  margin:0px;
+  padding:0px;
 }
 </style>
