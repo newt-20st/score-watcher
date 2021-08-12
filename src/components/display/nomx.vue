0aa23f1923f2be46e05c757231ddc704711e45df
newt <ysti2019.win2020.st@gmail.com>
2021-08-13 01:17:33 +0900

    add: nomxのUIの調整

--- src/components/display/nomx.vue
+++ src/components/display/nomx.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="control">
-    <h1>{{ data.config.correct }}o{{ data.config.wrong }}x</h1>
-    <router-link to="/config?type=nomx">設定に戻る</router-link>
-    <button @click="undo()">undo</button>
+    <h2>
+      {{ data.config.correct }}o{{ data.config.wrong }}x -
+      {{ data.log.length + 1 }}問目
+    </h2>
+    <div class="topMenu">
+      <div>
+        <router-link to="/config?type=nomx">設定に戻る</router-link>
+      </div>
+      <div>
+        <button @click="undo()">一つ戻す</button>
+      </div>
+    </div>
     <div class="players">
       <div
         class="player"
@@ -15,22 +24,25 @@
         <div class="playerName">
           {{ player.name }}
         </div>
-        <div class="playerCorrect" @click="correct(index)">
-          <div v-if="player.score.correct >= data.config.correct">WIN</div>
-          <div v-if="player.score.correct < data.config.correct">
-            {{ player.score.correct }}o
+        <div class="playerScore">
+          <div class="playerCorrect" @click="correct(index)">
+            <div v-if="player.score.correct >= data.config.correct">WIN</div>
+            <div v-if="player.score.correct < data.config.correct">
+              <span> {{ player.score.correct }}</span>
+            </div>
           </div>
-        </div>
-        <div class="playerWrong" @click="wrong(index)">
-          <div v-if="player.score.wrong >= data.config.wrong">LOSE</div>
-          <div v-if="player.score.wrong < data.config.wrong">
-            {{ player.score.wrong }}x
+          <div class="playerWrong" @click="wrong(index)">
+            <div v-if="player.score.wrong >= data.config.wrong">LOSE</div>
+            <div v-if="player.score.wrong < data.config.wrong">
+              <span>{{ player.score.wrong }}</span>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    {{ this.$store.state.config.format.nomx }}
     <div class="log">
+      <h3>ログ</h3>
+      <div v-if="data.log.length == 0">ここに得点履歴が表示されます。</div>
       <div
         class="eachLog"
         :name="index"
@@ -116,25 +128,89 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+h2 {
+  background-color: $base-color;
+  color: $back-color;
+  margin: 0;
+  padding: 0.5rem 1rem;
+  border-radius: 0 0 2rem 2rem;
+  text-align: center;
+}
+h3 {
+  color: $base-color;
+  margin: 0;
+}
+.topMenu {
+  margin: 0.5rem 1rem;
+  display: flex;
+  gap: 0.5rem;
+  div {
+    color: $back-color;
+    transition: all ease-in 0.3s;
+    a,
+    button {
+      cursor: pointer;
+      display: block;
+      padding: 0.5rem 1rem;
+      font-size: 1.5rem;
+      border: 1px solid $base-color;
+      border-radius: 2rem;
+      background-color: $base-color;
+      text-decoration: none;
+      color: $back-color;
+    }
+    &:hover {
+      opacity: 0.5;
+    }
+  }
+}
 .players {
   display: flex;
   justify-content: space-evenly;
-  font-size: clamp(8vh, 2rem, 8vw);
   .player {
     text-align: center;
     .playerName {
+      font-size: clamp(8vh, 2rem, 8vw);
       writing-mode: vertical-rl;
       vertical-align: top;
-      height: 50vh;
+      height: 40vh;
     }
-    .playerCorrect,
-    .playerWrong {
-      display: flex;
+    .playerScore {
       div {
+        font-size: 2rem;
+        position: relative;
+        margin: 1vh auto;
         cursor: pointer;
-        background-color: $base-color;
+        color: $back-color;
+        height: 10vh;
+        width: 10vh;
+        border-radius: 50%;
+        transition: all ease-in 0.3s;
+        span {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translateY(-50%) translateX(-50%);
+          -webkit-transform: translateY(-50%) translateX(-50%);
+        }
+        &:hover {
+          opacity: 0.5;
+        }
+      }
+      .playerCorrect {
+        background-color: $correct-color;
+      }
+      .playerWrong {
+        background-color: $wrong-color;
       }
     }
   }
+}
+.log {
+  margin: 0 1rem;
+  margin-top: 1rem;
+  padding: 1rem;
+  border: 0.3rem solid $base-color;
+  border-radius: 2rem;
 }
 </style>
