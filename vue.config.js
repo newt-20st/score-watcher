bf16b662616dd1ec07cbf1d203e91ec63d91d628
newt <ysti2019.win2020.st@gmail.com>
2021-08-13 14:02:05 +0900

    add: Github Actionによる自動リリース付け

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,24 @@ module.exports = {
     pluginOptions: {
         electronBuilder: {
             nodeIntegration: true,
+            builderOptions: {
+                "publish": {
+                    "provider": "github",
+                    "owner": "newt-20st",
+                    "repo": "scoreboard",
+                    "releaseType": "release"
+                },
+                productName: "my-application",
+                appId: "com.sample.myapplication",
+                win: {
+                    target: [
+                        {
+                            target: 'portable', // 'zip', 'nsis', 'portable'
+                            arch: ['x64'] // 'x64', 'ia32'
+                        }
+                    ]
+                }
+            }
         },
     },
     css: {
