8912fc4afffe37d5d294043c2c913e92389138ba
newt <ysti2019.win2020.st@gmail.com>
2021-09-05 14:43:43 +0900

    fix: AttackSurvivalのconfigにtimerのメニューを追加

--- src/mixin/configMenu.js
+++ src/mixin/configMenu.js
@@ -1,36 +1,6 @@
-const dialog = require("electron").remote.dialog;
-import fs from "fs";
 import Menu from '../components/ConfigMenu.vue'
 export default {
     components: { Menu },
     methods: {
-        configExport() {
-            const jsonData = this.$store.state.config.format[this.data.type];
-            jsonData.version = 0;
-            jsonData.exportTimestamp = new Date();
-            const options = {
-                title: "設定ファイルを保存",
-                filters: [{ name: "Documents", extensions: ["json"] }],
-            };
-            const result = dialog.showSaveDialogSync(options);
-            if (result) {
-                fs.writeFileSync(
-                    result,
-                    JSON.stringify(
-                        this.$store.state.config.format[this.data.type],
-                        null,
-                        "\t"
-                    )
-                );
-            }
-        },
-        quizUpdate() {
-            const quizRaw = document.getElementById("quizRaw").value.split("\n");
-            const dataArray = [];
-            for (let i = 0; i < quizRaw.length; i++) {
-                dataArray[i] = quizRaw[i].split(",");
-            }
-            this.data.quiz = dataArray;
-        },
     }
 }
