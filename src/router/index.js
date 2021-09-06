3f9b232790c103ecaf1a92385870798e64cc09c9
newt <ysti2019.win2020.st@gmail.com>
2021-09-06 23:52:39 +0900

    add: ページ遷移時にページトップまで戻るよう変更

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,14 @@ const routes = [
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
 })
 
 export default router
